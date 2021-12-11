4e75c4b949029eb2272191e7fd103b6036344792
Danny Lin <danny@kdrag0n.dev>
2021-12-12 07:28:09 +0800

    vendorsetup: Disable ABI checks
    ABI compatibility checks fail for several reasons:
      - The update to Clang 12 causes some changes, but no breakage has been
        observed in practice.
      - Switching to zlib-ng changes some internal structs, but not the public
        API.
    
    We may fix these eventually by updating the ABI specifications, but it's
    likely not worth the effort for us because of how many repos are affected.
    We would need to fork a lot of extra repos (thus increasing maintenance
    overhead) just to update the ABI specs.
    
    For now, just skip the ABI checks to fix build errors.
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,4 @@ done
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_HOSTNAME=projectarcana-build
 export BUILD_USERNAME=android-build
+export SKIP_ABI_CHECKS=true
