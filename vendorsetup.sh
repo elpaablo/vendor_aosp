a18273298d023a1e086a6ff3b8836942a33416eb
dlwlrma123 <alexfinhart@gmail.com>
2021-12-12 16:11:30 +0800

    vendorsetup: remove official device check

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-lunch_others_targets=()
-for device in $(python vendor/aosp/tools/get_official_devices.py)
-do
-    for var in eng user userdebug; do
-        lunch_others_targets+=("aosp_$device-$var")
-    done
-done
 
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_HOSTNAME=projectarcana-build
