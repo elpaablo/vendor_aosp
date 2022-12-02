20611eb2e93edc819d325a3ff348ec5fd982f6e3
elpaablo <donopwhile1@gmail.com>
2022-12-02 04:49:54 +0000

    Navigation bar: allow to hide IME space, as an extension of hide navigation hint [3/3]

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 # Branding
 $(call inherit-product, vendor/aosp/config/branding.mk)
 
-PRODUCT_BRAND ?= CustomExperience
+PRODUCT_BRAND ?= AlphaAOSP
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
@@ -118,7 +118,8 @@ PRODUCT_PACKAGES += \
 
 # Hide navigation bar hint
 PRODUCT_PACKAGES += \
-    NavigationBarNoHintOverlay
+    NavigationBarNoHintOverlay \
+    NavigationBarNoHintNoImeOverlay
 
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
